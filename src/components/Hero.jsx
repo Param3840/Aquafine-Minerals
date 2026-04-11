b854f5003818f85c15f4308ff52fe500c7de08b5
Paramveer Kumar <paramveersingh240303@gmail.com>
2026-04-11 17:34:36 +0530

    Updated Hero component image path

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,27 +5,29 @@ const Hero = () => {
   return (
     <div className="hero">
       <div className="hero-left">
-       <h1>Aquafine Minerals</h1>
-       <p className="ByShiva">By Shiva Industries</p>
+        <h1>Aquafine Minerals</h1>
+        <p className="ByShiva">By Shiva Industries</p>
         <p>
           Customised Packaged Drinking Water for Brands, Events & Businesses.
           Elevate your identity with every sip.
         </p>
-       <div className="hero-buttons">
-  <a 
-    href="https://wa.me/916202352446" 
-    target="_blank" 
-    rel="noopener noreferrer"
-  >
-    <button className="order-btn">🛒 Order Now</button>
-  </a>
-</div>
-
+        <div className="hero-buttons">
+          <a 
+            href="https://wa.me/916202352446" 
+            target="_blank" 
+            rel="noopener noreferrer"
+          >
+            <button className="order-btn">🛒 Order Now</button>
+          </a>
+        </div>
       </div>
 
       <div className="hero-right">
-        <img className="firstBottle" src="src\assets\firstBottlethree.png" alt="Aquafine Bottle" />
-       
+        <img 
+          className="firstBottle" 
+          src="/firstBottlethree.png" 
+          alt="Aquafine Bottle" 
+        />
       </div>
     </div>
   );
